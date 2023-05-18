ebe035e661b6981ba90c94bc5c923d66d18ae26e
Didi Walakutty <91764214+Didi-W@users.noreply.github.com>
2023-05-18 20:14:39 +0200

    update

--- push_swap/main.c
+++ push_swap/main.c
@@ -6,25 +6,22 @@
 /*   By: diwalaku <diwalaku@student.42.fr>            +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2023/05/04 22:07:12 by diwalaku      #+#    #+#                 */
-/*   Updated: 2023/05/18 20:12:39 by diwalaku      ########   odam.nl         */
+/*   Updated: 2023/05/18 20:14:18 by diwalaku      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-//int	main(int argc, char *argv[])
-int	main(void)
+int	main(int argc, char *argv[])
 {
-	char	c = 'a';
-	ft_printf("Di: char is: %c\n", c);
-	// t_stack *stack_a;
-	// t_stack	*stack_b;
+	t_stack *stack_a;
+	t_stack	*stack_b;
 
-	// if (argc < 2)
-	// 	exit(1);
-	// stack_a = create_stack();
-	// stack_b = create_stack();
-	// if (!stack_a || !stack_b)
-	// 	exit_error();
-	// check_arguments(argv);
+	if (argc < 2)
+		exit(1);
+	stack_a = create_stack();
+	stack_b = create_stack();
+	if (!stack_a || !stack_b)
+		exit_error();
+	check_arguments(argv);
 }
