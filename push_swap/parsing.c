3bec2ef65948f2c622fa15755d4aa1a88c52f803
Didi Walakutty <91764214+Didi-W@users.noreply.github.com>
2023-05-22 22:35:44 +0200

    log

--- push_swap/parsing.c
+++ push_swap/parsing.c
@@ -6,7 +6,7 @@
 /*   By: diwalaku <diwalaku@student.42.fr>            +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2023/05/17 19:56:09 by diwalaku      #+#    #+#                 */
-/*   Updated: 2023/05/19 22:01:18 by diwalaku      ########   odam.nl         */
+/*   Updated: 2023/05/22 21:10:01 by diwalaku      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -39,8 +39,30 @@ int	check_validity(char *str, int *nb)
 }
 
 // atoi, overflow and max-min numbers
-//
+// Whitespace before the numeric chars are typically ignored by atoi.
+// It starts parsing the string from the first non-whitespace char.
 int	atoi_and_overflow(char *str, int *nb)
 {
+	int	x;
+	int	n;
+	int	multx;
 
+	x = 0;
+	n = 0;
+	multx = 1;
+	while ((str[x] == '\t' || str[x] == '\n' || str[x] == '\v' || \
+	str[x] == '\f' || str[x] == '\r' || str[x] == ' '))
+		x++;
+	if (str[x] == '-' || str[x] == '+')
+	{
+		if (str[x] == '-')
+			multx *= -1;
+		x++;
+	}
+	while (str[x] >= '0' && str[x] <= '9')
+	{
+		n = (n * 10) + (str[x] - '0');
+		x++;
+	}
+	if (s)
 }
