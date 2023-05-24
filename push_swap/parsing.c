2bdd0b72ffecc7f66cfea3e011bce9d15e17b6e5
Didi Walakutty <91764214+Didi-W@users.noreply.github.com>
2023-05-24 21:00:13 +0200

    Updated atoi_overflow function

--- push_swap/parsing.c
+++ push_swap/parsing.c
@@ -6,7 +6,7 @@
 /*   By: diwalaku <diwalaku@student.42.fr>            +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2023/05/17 19:56:09 by diwalaku      #+#    #+#                 */
-/*   Updated: 2023/05/22 21:10:01 by diwalaku      ########   odam.nl         */
+/*   Updated: 2023/05/24 20:59:24 by diwalaku      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -38,17 +38,18 @@ int	check_validity(char *str, int *nb)
 	return (1);
 }
 
-// atoi, overflow and max-min numbers
-// Whitespace before the numeric chars are typically ignored by atoi.
-// It starts parsing the string from the first non-whitespace char.
+// atoi, overflow and max-min numbers.
+// Start parsing the string from the first non-whitespace char, because whitespace is ignored in atoi.
+// INT_MIN is -...648, INT_MAX is ...647, but in case of INT_MIN (-), we still need to multx it with -1.
+// At first, the int will be bigger than INT_MAX (...648 instead of the max ...647), so I made it an edgecase.
+// the multx=1 check is in case the string is bigger that INT_MAX. It'll also automatically turn nb in a negative (overflow).
 int	atoi_and_overflow(char *str, int *nb)
 {
 	int	x;
-	int	n;
 	int	multx;
 
+	*nb = 0;
 	x = 0;
-	n = 0;
 	multx = 1;
 	while ((str[x] == '\t' || str[x] == '\n' || str[x] == '\v' || \
 	str[x] == '\f' || str[x] == '\r' || str[x] == ' '))
@@ -61,8 +62,13 @@ int	atoi_and_overflow(char *str, int *nb)
 	}
 	while (str[x] >= '0' && str[x] <= '9')
 	{
-		n = (n * 10) + (str[x] - '0');
+		*nb += str[x] - '0';
+		if (*nb < 0 && (*nb != INT_MIN || multx = 1))
+			exit_error();
+		if (str[x + 1] >= '0' && str[x + 1] <= '9')
+			*nb *= 10;
 		x++;
 	}
-	if (s)
+	*nb *= multx;
+	return (1);
 }
